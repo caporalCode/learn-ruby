1045464a0c346ac05f90b0581dc5e6756410ce27
caporalCode <idealprogrammeur@gmail.com>
2024-02-19 18:26:00 +0100

    learn string

---
+++ main.rb
@@ -0,0 +1,107 @@
+# load 'hello.rb'
+
+# Multiline commad
+=begin
+puts "Hello World!"
+p 'Rivaland'
+print "I'm learning Ruby!"
+=end
+
+# name = "Rivaland"
+# puts name
+
+# num = 123 
+# cond = true
+# puts name, num, cond
+
+# puts "Hello, #{name} you have #{num} dollars."
+
+# print "Enter your name: "
+# name = gets.chomp()
+
+# print "Enter your age: "
+# age = gets.chomp().to_i
+
+# puts "Your name is #{name} and your age is #{age} years old."
+
+
+# str = "Hello, World!"
+# str1= 'this is a string'
+
+# puts str
+# puts str1
+
+# str3 = "I'm learning Ruby!
+# multiline strings
+# in Ruby"
+
+# puts str3
+
+# str4 = %/this is a string 
+# in Ruby/
+
+# puts str4
+
+# str5 = "hello"
+
+# puts str5.size
+# puts str5.length
+# puts str5.upcase
+# puts str5.downcase
+# puts str5.reverse
+# puts str5.upcase.reverse
+# puts str5.include? "hel"
+
+
+# puts "hello" + ", how are you?"
+
+# puts "hello".concat ", how are you...?"
+
+# str6= "hi"
+
+# puts str6
+
+# str6 = str6 << " again"
+# puts str6
+# str6.freeze
+# str6 = str6 << " again"
+
+
+# puts "hello" == "hello"
+
+# puts "abc".eql? "123"
+
+# msg = "Hello from automation step by step"
+# puts msg
+
+# puts msg['Hello']
+
+# puts msg[0]
+# puts msg[0,4]
+# puts msg[0..20]
+# puts msg[0, msg.length]
+# puts msg[-4]
+# puts msg.length
+
+# puts 10 + 20
+# puts 20 - 10
+# puts 10 * 20
+# puts 20 / 10
+# puts 20 % 10
+# puts 2 ** 3
+
+
+# puts (2 == 4)
+# puts (2 != 4)
+# puts (2 > 4)
+# puts (2 < 4)
+# puts (2 >= 4)
+# puts (2 <= 4)
+
+a = 10
+b = 20
+
+c = (a > b) ? a : b
+
+
+puts "Greater value of a and b is c = #{c}"
